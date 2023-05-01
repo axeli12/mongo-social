94c5a563cba285ae82a0165bbe8180c2448a88b9
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-05-01 17:58:52 -0600

    added server and database connection

---
+++ config/connection.js
@@ -0,0 +1,6 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1/networkDB';
+
+connect(connectionString);
+ module.exports = connection;
